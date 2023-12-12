b3badc8bfe8ed9005c77c007962605ae779b590e
MazidulHasan <md.mazidulhasan1@gmail.com>
2023-12-13 01:53:56 +0600

    Update the debug as Falase in main.py

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ def process_data():
     return 'Data processing initiated.'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
     # app.run(host='0.0.0.0', port=5000, debug=True)
